060cb2d2b6954ca488e5cb9f08da6e5225990dac
tajimayuki <softte2su77@gmail.com>
2026-04-07 22:17:43 +0900

    Initial Commit

---
+++ DailyBudget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DailyBudget
+//
+//  Created by 田島佑紀 on 2026/04/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DailyBudget/DailyBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyBudgetApp.swift
+//  DailyBudget
+//
+//  Created by 田島佑紀 on 2026/04/07.
+//
+
+import SwiftUI
+
+@main
+struct DailyBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
